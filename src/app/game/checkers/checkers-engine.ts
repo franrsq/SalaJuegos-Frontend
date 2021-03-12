1fb665e6c9a3760822ad67de382653d55ddccf88
Jairo <pachecocamposjairo@gmail.com>
2021-03-11 18:40:56 -0600

    posibleJugada

--- src/app/game/checkers/checkers-engine.ts
+++ src/app/game/checkers/checkers-engine.ts
@@ -10,7 +10,7 @@ export class CheckersEngine extends Engine {
 
     initGame(board: Board) {
         this.board = board;
-
+        board.board[3][5].piece = new BlackPiece(); // mae croki playo esto lo pusimos nosotros para probar cosas, si quiere se lo hecha :)
         for (let i = 0; i < board.board.length; i++) {
             for (let j = 0; j < board.board[i].length; j++) {
                 this.element = board.board[i][j]; // fila columna 
@@ -27,8 +27,72 @@ export class CheckersEngine extends Engine {
     }
 
     click(row, column) {
-        let spaces = [this.board.board[row][column]];
+        let spaces = this.posibleJugada(row, column);
         this.board.removeHighlight();
-        this.board.highlight(spaces);
+        if (spaces != []) {
+            this.board.highlight(spaces);
+        }
+    }
+
+    posiblesEspaciosNegros(row, colum): Space[] {
+        let spaces: Space[] = [];
+        if (this.board.board[row][colum].piece != null) {
+            if (colum != (this.board.board[0].length - 1)) {
+                if (this.board.board[row + 1][colum + 1].piece == null) { //Si en el lugar a mover no hay una pieza
+                    spaces = [this.board.board[row + 1][colum + 1]]; // entonces es un espacio disponible
+                } else if (this.board.board[row + 1][colum + 1].piece.getPieceImg().includes("black")) { //si en el lugar a mover hay una pieza negra
+                    console.log("no se puede jugar hay un aliado derecha") // no se puede jugar ahi
+                } else {
+                    console.log("es enemigo") //aqui va a ir la logica de si es un enemigo
+                }
+            }
+            if (colum != 0) {
+                if (this.board.board[row + 1][colum - 1].piece == null) {
+                    spaces.push(this.board.board[row + 1][colum - 1]); // entonces es un espacio disponible
+                } else if (this.board.board[row + 1][colum - 1].piece.getPieceImg().includes("black")) {
+                    console.log("no se puede jugar hay un aliado izquierda")
+                } else {
+                    console.log("es enemigo") //aqui va a ir la logica de si es un enemigo
+                }
+            }
+        }
+        return spaces;
+    }
+
+    posiblesEspaciosRojos(row, colum): Space[] {
+        let spaces: Space[] = [];
+        if (this.board.board[row][colum].piece != null) {
+            if (colum != (this.board.board[0].length - 1)) {
+                if (this.board.board[row - 1][colum + 1].piece == null) { //Si en el lugar a mover no hay una pieza
+                    spaces = [this.board.board[row - 1][colum + 1]]; // entonces es un espacio disponible
+                } else if (this.board.board[row - 1][colum + 1].piece.getPieceImg().includes("red")) { //si en el lugar a mover hay una pieza negra
+                    console.log("no se puede jugar hay un aliado derecha") // no se puede jugar ahi
+                } else {
+                    console.log("es enemigo") //aqui va a ir la logica de si es un enemigo
+                }
+            }
+            if (colum != 0) {
+                if (this.board.board[row - 1][colum - 1].piece == null) {
+                    spaces.push(this.board.board[row - 1][colum - 1]); // entonces es un espacio disponible
+                } else if (this.board.board[row - 1][colum - 1].piece.getPieceImg().includes("red")) {
+                    console.log("no se puede jugar hay un aliado izquierda")
+                } else {
+                    console.log("es enemigo") //aqui va a ir la logica de si es un enemigo
+                }
+            }
+        }
+        return spaces;
+    }
+
+    posibleJugada(row, colum): Space[]{
+        let spaces: Space[] = [];
+        if(this.board.board[row][colum].piece != null){
+            if(this.board.board[row][colum].piece.getPieceImg().includes("red")){
+                spaces = this.posiblesEspaciosRojos(row,colum);
+            }else{
+                spaces = this.posiblesEspaciosNegros(row,colum);
+            }
+        }
+        return spaces;
     }
 }
